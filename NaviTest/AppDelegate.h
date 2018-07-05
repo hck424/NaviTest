7389b9a4f9ad2872b02f8c5036fd2dcf76dbc765
학철 <hagcheol@iphone-devpsynetcokrui-macbook-pro.local>
2018-07-05 17:38:01 +0900

    Initial Commit

---
+++ NaviTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NaviTest
+//
+//  Created by 학철 on 2018. 7. 5..
+//  Copyright © 2018년 학철. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NaviTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NaviTest
+//
+//  Created by 학철 on 2018. 7. 5..
+//  Copyright © 2018년 학철. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
